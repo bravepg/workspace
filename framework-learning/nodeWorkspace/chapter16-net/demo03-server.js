99599bccbee5338f747bb26526434e6add72804c
gaopeng <gaopeng19960108@gmail.com>
2018-12-18 15:59:44 +0800

    :recycle:理解socket

--- framework-learning/nodeWorkspace/chapter16-net/demo03-server.js
+++ framework-learning/nodeWorkspace/chapter16-net/demo03-server.js
@@ -8,7 +8,7 @@ const net = require('net');
 const server = net.createServer();
 server.on('connection', (socket) => {
 	socket.pipe(process.stdout);
-  	socket.write('data from server');
+  socket.write('data from server');
 });
 
 server.listen(3000, () => {
