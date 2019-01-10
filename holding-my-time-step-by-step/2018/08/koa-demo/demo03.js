657638cf4894c9da94c99a733999fb164a20b116
gaopeng <gaopeng19960108@gmail.com>
2019-01-10 15:54:35 +0800

    :ok_hand: koa学习补充

--- holding-my-time-step-by-step/2018/08/koa-demo/demo03.js
+++ holding-my-time-step-by-step/2018/08/koa-demo/demo03.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
 const app = new Koa();
-const route = require('koa-route');
+const Router = require('koa-router');
+
+var router = new Router();
 
 const about = ctx => {
   ctx.response.type = 'html';
@@ -11,6 +13,20 @@ const main = ctx => {
   ctx.response.body = 'Hello World';
 };
 
-app.use(route.get('/', main));
-app.use(route.get('/about', about));
+// 带有名称的路由
+router.get('user', '/users/:id', (ctx, next) => {
+  ctx.response.body = 'Hello World';
+});
+
+// router.url(name, params, [options]) ⇒ String | Error
+console.log(router.url('user', 3));
+
+console.log(router.route('user'))  // 返回一个 layer 查看，跟 express 中的功能很不一样
+
+// 利用 Router 直接生成路由
+// Router.url(path, params [, options]) ⇒ String
+console.log(Router.url('/users/:id', {id: 1}));
+
+app.use(main);
+app.use(router.routes);  // 使用路由
 app.listen(3000);
