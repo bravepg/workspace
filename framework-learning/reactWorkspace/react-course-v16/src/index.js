f0f2840d2ccf19b2036b40d0b4cabec85bf19b93
gaopeng <gaopeng19960108@gmail.com>
2019-01-29 16:07:49 +0800

    学习react

--- framework-learning/reactWorkspace/react-course-v16/src/index.js
+++ framework-learning/reactWorkspace/react-course-v16/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 // import App from './App';
-import App from './examples/Image';
+import App from './examples/Redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
