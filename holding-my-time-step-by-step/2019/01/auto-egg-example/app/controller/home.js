056e4f4de0c14243dc45150ee27dfa2587d9f947
gaopeng <gaopeng19960108@gmail.com>
2019-02-23 12:03:25 +0800

    :recycle: refactor code

--- holding-my-time-step-by-step/2019/01/auto-egg-example/app/controller/home.js
+++ holding-my-time-step-by-step/2019/01/auto-egg-example/app/controller/home.js
@@ -1,11 +1,29 @@
 'use strict';
 
-const Controller = require('egg').Controller;
+// const Controller = require('egg').Controller;
 
-class HomeController extends Controller {
-  async index() {
-    this.ctx.body = 'hi, egg';
-  }
-}
+// class HomeController extends Controller {
+//   async index() {
+//     this.ctx.body = 'hi, egg';
+//   }
+// }
 
-module.exports = HomeController;
+// module.exports = HomeController;
+
+exports.index = async (app, ctx) => {
+  console.log(ctx, 'ctx');
+  app.body = JSON.stringify(app);
+};
+
+
+// module.exports = app => {
+
+//   return class HomeController extends app.Controller {
+
+//     async index() {
+//       this.ctx.body = JSON.stringify(app);
+//     }
+
+//   };
+
+// };
--- holding-my-time-step-by-step/2019/01/auto-egg-example/app/router.js
+++ holding-my-time-step-by-step/2019/01/auto-egg-example/app/router.js
@@ -4,9 +4,9 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
-  app.router.get('/search', app.controller.search.index);
-  app.router.post('/search', app.controller.search.post);
-  app.router.post('/user', app.controller.search.create);
+  const { router } = app;
+  router.get('/', 'home.index');
+  router.get('/search', 'search.index');
+  app.router.post('/search', 'search.post');
+  app.router.post('/user', 'search.create');
 };
